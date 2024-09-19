71962618fe14e5c13a7505f0f6ea15e033f10141
CaoTai1710 <23521368@gm.uit.edu.vn>
2024-09-19 10:04:37 +0700

    Assignment2

---
+++ openfiledialog_picturebox/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace openfiledialog_picturebox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            btnOpen = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(476, 414);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            btnOpen.Location = new Point(511, 368);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(94, 58);
+            btnOpen.TabIndex = 1;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(638, 450);
+            Controls.Add(btnOpen);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+        private Button btnOpen;
+    }
+}
---
+++ openfiledialog_picturebox/Form1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openfiledialog_picturebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = "Anh |*.jpg; *.gif; *.png; |All File|*.*";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                // MessageBox.Show(openFile.SafeFileName);
+                pictureBox1.Image = Image.FromFile(openFile.FileName);
+            }
+        }
+    }
+}
